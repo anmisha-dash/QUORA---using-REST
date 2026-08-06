2f1f3f2566664a08c9e535c5a8458ff43b4fbe35
Anmisha Dash <dashanmisha@gmail.com>
2026-08-06 11:09:31 +0530

    all the posts using GET-REST

---
+++ index.js
@@ -0,0 +1,34 @@
+const express = require("express");
+const app = express();
+const port = 8080;
+const path = require("path");
+
+app.use(express.urlencoded({extended:true}));
+
+app.set("view engine","ejs");
+app.set("views",path.join(__dirname,"views"));
+
+app.use(express.static(path.join(__dirname,"public")));
+
+let posts = [
+    {
+        username : "Anmisha",
+        content : "enjoy coding"
+    },
+    {
+        username : "Shradha",
+        content : "I Love coding!"
+    },
+    {
+        username : "apnacollge",
+        content : "hardwork always pays off"
+    }
+];
+
+app.get("/posts",(req,res)=>{
+    res.render("index.ejs",{posts});
+});
+
+app.listen(port,()=>{
+    console.log(`app is listening at ${port}`);
+});
