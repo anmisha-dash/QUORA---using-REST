1f7e63c2666a958de8f5cbb6ab8c0f3470ed85ba
Anmisha Dash <dashanmisha@gmail.com>
2026-08-06 18:25:43 +0530

    details of each post using uuid

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require("express");
 const app = express();
 const port = 8080;
 const path = require("path");
+const {v4:uuidv4} = require('uuid');
 
 app.use(express.urlencoded({extended:true}));
 
@@ -12,14 +13,17 @@ app.use(express.static(path.join(__dirname,"public")));
 
 let posts = [
     {
+        id : uuidv4(),
         username : "Anmisha",
         content : "enjoy coding"
     },
     {
+        id : uuidv4(),
         username : "Shradha",
         content : "I Love coding!"
     },
     {
+        id : uuidv4(),
         username : "apnacollge",
         content : "hardwork always pays off"
     }
@@ -31,11 +35,18 @@ app.get("/posts/new",(req,res)=>{
 
 app.post("/posts",(req,res)=>{
     let {username,content}=(req.body);
-    posts.push({username,content});
+    let id = uuidv4();
+    posts.push({id,username,content});
     res.redirect("/posts");
 
     // res.send("post request working");
-})
+});
+app.get("/posts/:id",(req,res)=>{
+    let {id} = req.params;
+    let post = posts.find((p)=> id===p.id);
+    console.log(post);
+    res.render("show.ejs",{post});
+});
 
 app.get("/posts",(req,res)=>{
     res.render("index.ejs",{posts});
