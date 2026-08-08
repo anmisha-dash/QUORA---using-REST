fa98e53ff4d81c3046ec2e61e1f2dccb3134e335
Anmisha Dash <dashanmisha@gmail.com>
2026-08-08 09:21:37 +0530

    added delete option using DELETE

--- index.js
+++ index.js
@@ -68,6 +68,11 @@ app.get("/posts/:id/edit",(req,res)=>{
     let post = posts.find((p)=> id===p.id);
     res.render("edit.ejs",{post});
 });
+app.delete("/posts/:id",(req,res)=>{
+    let {id}=req.params;
+    posts = posts.filter((p)=> id!== p.id);
+    res.redirect("/posts");
+})
 
 app.listen(port,()=>{
     console.log(`app is listening at ${port}`);
